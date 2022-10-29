19fa951b18a5e9a22c3257f3d05225ed6ca435f1
Eraikun <eray.alpdogan@zo.uni-heidelberg.de>
2022-10-29 17:43:07 +0200

    applusplusapp added to our global django project and Employee model created

--- aplusplus_venv/aplusplus/aplusplus/settings.py
+++ aplusplus_venv/aplusplus/aplusplus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aplusplusapp.apps.AplusplusappConfig',
     'rest_framework',
 ]
 
--- aplusplus_venv/aplusplus/aplusplusapp/models.py
+++ aplusplus_venv/aplusplus/aplusplusapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    id = models.AutoField(
+            primary_key=True
+        )
+    # Fields
+    Name = models.CharField(max_length=255, null=False)
+    Hourly_Rate = models.FloatField(null=False)
+
+    class Meta:
+        db_table = "Employee"
