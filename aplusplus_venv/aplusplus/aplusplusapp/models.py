eba111f722519ec23f72149278702924562c9445
Eraikun <eray.alpdogan@zo.uni-heidelberg.de>
2022-11-01 11:21:14 +0100

    multiple objects with simple relations created which relates to the assignment given
    To-Do: test relationships

--- aplusplus_venv/aplusplus/aplusplusapp/models.py
+++ aplusplus_venv/aplusplus/aplusplusapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
+    """Represents an employee who can work in multiple teams."""
     id = models.AutoField(
             primary_key=True
         )
@@ -15,3 +16,54 @@ class Employee(models.Model):
 
     class Meta:
         db_table = "employee"
+
+class Team(models.Model):
+    """Represents a Team."""
+    # Fields
+    id = models.AutoField(
+            primary_key=True
+        )
+    teamTitle = models.CharField(max_length=255, null=False)
+
+    
+class TeamLeader(models.Model):
+    """Represents a TeamLeader."""
+    # Fields
+    id = models.AutoField(
+            primary_key=True
+        )
+    leader = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='leader')
+    #A Team can only have one Leader
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+class TeamEmployee(models.Model):
+    """Represents a TeamEmployee."""
+    # Fields
+    id = models.AutoField(
+            primary_key=True
+        )
+    # 1:1 relation: one employee can be part of many teams
+    member = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='member')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+
+class WorkArrangement(models.Model):
+    """Represents a Work Assignment which then worked by 1 team."""
+    # Fields
+    id = models.AutoField(
+            primary_key=True
+        )
+    class Time(models.TextChoices):
+        FT = 'Full Time'
+        PT = 'Part Time'
+        PT75 = 'Part Time 75%'
+        PT50 = 'Part Time 50%'
+        PT25 = 'Part Time 25%'
+
+    employee = models.ForeignKey(TeamEmployee, on_delete=models.CASCADE, related_name='processed_by_employee')
+    # work title
+
+    # work title
+    workTitle = models.CharField(max_length=255, null=False)
+    # is employee works full time on this work assignment? 
+    workedTime = models.CharField(max_length=255, choices=Time.choices)
