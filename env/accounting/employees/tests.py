f765b469558a0c121efedc40b2ee5b1d88d18a1c
Eraikun <eray.alpdogan@zo.uni-heidelberg.de>
2022-11-04 23:34:30 +0100

    startet on unit tests

--- env/accounting/employees/tests.py
+++ env/accounting/employees/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from rest_framework import status
 # Create your tests here.
+from rest_framework.test import APIRequestFactory, APITestCase
+
+from .models import Employee, Team, TeamLeader, TeamMember, WorkArrangement
+from .serializers import (EmployeeSerializer, TeamSerializer, TLSerializer,
+                          TMSerializer, WASerializer)
+
+# Using the standard RequestFactory API to create a form POST request
+factory = APIRequestFactory()
+request = factory.post('/api/teamleader/', {'name': 'Ömer', 'teamAffiliation': 1, 'hourlyRate':30.0})
